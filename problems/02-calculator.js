35678ed7ace93261c4a0ee4dcc3baa020850e75c
Kim <kimharris2018@gmail.com>
2023-02-14 13:52:26 -0600

    calculator problem

--- problems/02-calculator.js
+++ problems/02-calculator.js
@@ -1,4 +1,32 @@
-// Your code here
+class Calculator{
+	constructor(total){
+		this.total = 0
+	}
+
+	add(num) {
+		this.total += num;
+		return this.total;
+	  }
+	
+	  subtract(num) {
+		this.total -= num;
+		return this.total;
+	  }
+	
+	  divide(num) {
+		if (num === 0) {
+		  throw new Error("Cannot divide by zero");
+		}
+		this.total /= num;
+		return this.total;
+	  }
+	
+	  multiply(num) {
+		this.total *= num;
+		return this.total;
+	  }
+	}
+
 
 
 /*****************************************************************************/
